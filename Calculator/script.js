9a6b78df599a808eb68bf0d22fad98b0118a6d20
Jesus Cantu <chuy_cr11@hotmail.com>
2022-03-19 15:50:55 -0600

    Added base style to calculator

---
+++ Calculator/script.js
@@ -0,0 +1,11 @@
+function Operate(l, operator, r) {
+    if (operator === '+')
+        return l + r;
+    if (operator === '-')
+        return l - r;
+    if (operator === '*')
+        return l * r;
+    if (operator === '/')
+        return l / r;
+}
+
